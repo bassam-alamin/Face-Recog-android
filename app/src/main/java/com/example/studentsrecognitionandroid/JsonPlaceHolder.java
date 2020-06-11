638e64ddc78573b605fb879fe9724c4fd9026deb
bassam-alamin <bassamalchinsky96@gmail.com>
2020-06-12 00:22:03 +0300

    initial commit already with login page

---
+++ app/src/main/java/com/example/studentsrecognitionandroid/JsonPlaceHolder.java
@@ -0,0 +1,35 @@
+package com.example.studentsrecognitionandroid;
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface JsonPlaceHolder {
+
+
+    //get a single user ,single meaning an java object
+    @GET("user/{id}")
+    Call<Users> getUser(@Path("id") int id);
+
+
+    @POST("login/")
+    Call<Users> loginUser(@Body Users users);
+
+//    @POST("departments/")
+//    Call<Department> createDepartment(@Body Department department);
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/studentsrecognitionandroid/LoginActivity.java
@@ -0,0 +1,85 @@
+package com.example.studentsrecognitionandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private JsonPlaceHolder jsonPlaceHolder;
+    private Users users;
+
+    private EditText username;
+    private EditText password;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:8000/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+
+        jsonPlaceHolder = retrofit.create(JsonPlaceHolder.class);
+
+
+        Button btn = findViewById(R.id.loginButton);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+
+
+
+
+
+    }
+
+    public void loginUser(){
+        username = findViewById(R.id.username);
+        String usern = username.getText().toString();
+        password = findViewById(R.id.password);
+
+        String pass = password.getText().toString();
+
+
+        Users users = new Users(usern,pass);
+
+
+        Call<Users> call = jsonPlaceHolder.loginUser(users);
+
+        call.enqueue(new Callback<Users>() {
+            @Override
+            public void onResponse(Call<Users> call, Response<Users> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<Users> call, Throwable t) {
+
+            }
+        });
+
+
+
+
+    }
+}
