0f6b921e0c973b503053699954d8e0cdc96b1bcf
bassam-alamin <bassamalchinsky96@gmail.com>
2020-06-16 22:03:10 +0300

    added the Navigation drawer

--- app/src/main/java/com/example/studentsrecognitionandroid/MainActivity.java
+++ app/src/main/java/com/example/studentsrecognitionandroid/MainActivity.java
@@ -32,6 +32,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },4000);
+        },1000);
     }
 }
--- app/src/main/java/com/example/studentsrecognitionandroid/WelcomeLecture.java
+++ app/src/main/java/com/example/studentsrecognitionandroid/WelcomeLecture.java
@@ -1,16 +1,27 @@
 package com.example.studentsrecognitionandroid;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
-public class WelcomeLecture extends AppCompatActivity {
-    private TextView welcome;
+import com.google.android.material.navigation.NavigationView;
+
+public class WelcomeLecture extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+//    private TextView welcome;
     private SharedPreferences sharedPreferences;
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +40,43 @@ public class WelcomeLecture extends AppCompatActivity {
 
 
 
-        welcome = findViewById(R.id.welcome);
+//        welcome = findViewById(R.id.welcome);
+//
+//        welcome.setText(value+"Welcome home");
 
-        welcome.setText(value+"Welcome home");
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer);
+        NavigationView navigationView = findViewById(R.id.navigationview);
+
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
 
 
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        if(menuItem.getItemId() == R.id.home){
+
+        }
+
+
+        if(menuItem.getItemId() == R.id.Supervise){
+
+        }
+
+        if(menuItem.getItemId() == R.id.reports){
+
+        }
+
+
+        return true;
     }
 }
